8f7ee9b537725fa2134b6ac722d1a5b3405fd3af
vkarpov <brainysmith@gmail.com>
2014-05-22 17:04:12 +0400

    JWT ready

--- src/main/scala/com/identityblitz/jwt/JwsToolkit.scala
+++ src/main/scala/com/identityblitz/jwt/JwsToolkit.scala
@@ -67,54 +67,51 @@ trait JwsToolkit extends AlgorithmsKit with JwtToolkit with JwkToolkit {
     override def toString: String = (values + ("alg" -> alg.toString)).toString
   }
 
+  val jwtPattern = """^([A-Za-z0-9_-]+\.([A-Za-z0-9_-]+))\.([A-Za-z0-9_-]*)$""".r
+
   val none = Algorithm[JWS, JWSNoneBuilder]("none",
     (alg, hdr, tkn) => {
-      if(!tkn.endsWith("."))
-        throw new IllegalStateException("token has wrong format")
-      tkn.split("\\.").toList match {
-        case _ :: pl :: Nil => (new JWSNone(hdr), Base64.decodeBase64(pl))
-        case _ => throw new IllegalStateException("token has wrong format")
+      jwtPattern findFirstIn tkn match {
+        case Some(jwtPattern(_, pl, _)) => (new JWSNone(hdr), Base64.decodeBase64(pl))
+        case None => throw new IllegalStateException("token has wrong format")
       }},
-    (hdr, pl) => hdr.asBase64 + "." + Base64.encodeBase64URLSafeString(pl),
+    serializeNoneJws,
     new JWSNoneBuilder(_)
   )
 
   val HS256 = Algorithm[JWS, JWSBuilder]("HS256",
-    deserializeJWS(cryptoService),
-    serializeJWS,
+    deserializeJws(cryptoService),
+    serializeJws,
     new JWSBuilder(_)
   )
 
-  def serializeJWS(hdr: JWS, pl: Array[Byte]) = {
+  def serializeNoneJws(hdr: JWS, pl: Array[Byte]) = hdr.asBase64 + "." + Base64.encodeBase64URLSafeString(pl) + "."
+
+  def serializeJws(hdr: JWS, pl: Array[Byte]) = {
     val hpl = hdr.asBase64 + "." + Base64.encodeBase64URLSafeString(pl)
     val signature = Base64.encodeBase64URLSafeString(hdr.crypto.sign(hdr.alg.name, null, hpl.getBytes("US-ASCII")))
     hpl + "." + signature
   }
 
-  def deserializeJWS(cs: CryptoService)(alg: Algorithm[JWS, _], hdr: JObj, tkn: String) = {
-    tkn.split("\\.").toList match {
-      case h :: pl :: mac ::Nil =>
+  def deserializeJws(cs: CryptoService)(alg: Algorithm[JWS, _], hdr: JObj, tkn: String) = {
+    jwtPattern findFirstIn tkn match {
+      case Some(jwtPattern(hpl, pl, s)) =>
         val jws = new JWS(alg, hdr)(cs)
-        if(!cs.verify(alg.name, null, (h + "." + pl).getBytes("US-ASCII"),Base64.decodeBase64(mac)))
-          throw new IllegalStateException("MAC is wrong.")
+        if(!cs.verify(alg.name, null, hpl.getBytes("US-ASCII"),Base64.decodeBase64(s)))
+          throw new IllegalStateException("Signature is wrong.")
         (jws, Base64.decodeBase64(pl))
-      case _ => throw new IllegalStateException("token has wrong format")
+      case None => throw new IllegalStateException("token has wrong format")
     }
   }
 
 
 
-
-
-
-
-
-
-
-
-
   private sealed class JWSNone(private val values: JObj) extends JWS(none, values)(null)
 
+  /**
+   * JWS header builder.
+   * @param _alg - signature algorithm used to protect token
+   */
   sealed class JWSNoneBuilder(_alg: Algorithm[JWS, JWSNoneBuilder]) {
     def header(hs: (String, JVal)*) = new PayloadBuilder[JWS] {
       val header: JWS = new JWSNone(JObj(hs))
